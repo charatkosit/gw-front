ae65ffe5aa7e36986cd40ed436face84d345d5d6
charatkosit <charatkosit@gmail.com>
2024-08-13 17:38:32 +0700

    rev0.1.30

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   version: 'prod:0.0.1',
-  release:'0.1.29',
+  release:'0.1.30',
   // loginResult: "loginResult",
   // isLoggedIn: false,   // ตรวจสอบสถานะล็อกอินเริ่มต้นจาก localStorage ใน AuthService ใหม่
 };
